3bc636c2fd45ae0512a6b3b0232169356db4d5f4
ahaoboy <504595380@qq.com>
2026-01-17 22:22:07 +0800

    ci

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -88,6 +88,7 @@ async fn read_file(path: &str, app_handle: tauri::AppHandle) -> Result<Vec<u8>, 
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
