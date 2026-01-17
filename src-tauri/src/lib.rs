15bcc1f83494e9c5bc4a5697f06e670f5ba66ab0
ahaoboy <504595380@qq.com>
2026-01-17 22:36:57 +0800

    ci

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -86,12 +86,19 @@ async fn read_file(path: &str, app_handle: tauri::AppHandle) -> Result<Vec<u8>, 
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
+    let mut builder = tauri::Builder::default()
         .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_fs::init());
+
+    // Only include these plugins on desktop platforms
+    #[cfg(not(any(target_os = "android", target_os = "ios")))]
+    {
+        builder = builder
+            .plugin(tauri_plugin_updater::Builder::new().build())
+            .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}));
+    }
+
+    builder
         .invoke_handler(tauri::generate_handler![
             extract_audios,
             app_dir,
